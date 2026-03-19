6855169fb34d393cfe2c4590ab24777244ee7972
JoeDB993 <131511247+JoeDB993@users.noreply.github.com>
2026-03-18 19:48:48 -0600

    Instalación de cors

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+const cors = requier('cors');
 
 const authRoute = require('./routes/authRoute');
 
 app.use(express.json());
-
+app.use(cors());
 
 
 app.use('/', authRoute);
