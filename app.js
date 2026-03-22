4613d41ddd4e05bb4bcdbcdaa693d4590ee65457
Marlinish98 <alex2015drum@gmail.com>
2026-03-22 17:21:41 -0600

    Adición de ventasRoute

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
-
+const cors = require('cors');
 const authRoute = require('./routes/authRoute');
+const ventasRoute = require('./routes/ventasRoute');
 
 app.use(express.json());
 
 
-
 app.use('/', authRoute);
-
-
-
-
+app.use('/', ventasRoute);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
